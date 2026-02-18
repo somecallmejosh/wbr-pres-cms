9e0da14c9efcc62ac25821b600690aa11528f7e5
somecallmejosh <josh@thebrileys.com>
2026-02-17 22:26:48 -0500

    remove custom exception handling for routes.

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,5 @@ module WbrPresCms
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.exceptions_app = routes
   end
 end

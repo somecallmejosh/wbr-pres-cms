26ab8cdbf520ad8e2246cb7df4f121cb991eed21
somecallmejosh <josh@thebrileys.com>
2026-02-17 22:24:21 -0500

    Lazy evaluation of route references.

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module WbrPresCms
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.exceptions_app = self.routes
+    config.exceptions_app = routes
   end
 end

39126258be8d730fada835ec8a479cf988fc4dfb
somecallmejosh <josh@thebrileys.com>
2026-02-23 14:47:13 -0500

    A little cleanup.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # Admin user
 User.find_or_create_by!(email_address: "josh@thebrileys.com") do |user|
-  user.password = "password"
-  user.password_confirmation = "password"
+  user.password = ""
+  user.password_confirmation = ""
 end
 
 # Members (24 total, at least one birthday per month)
